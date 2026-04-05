6807aa4b77e00c208069d266ce40ae8839200098
Alexander Dao <alexanderngocdao@gmail.com>
2026-04-05 09:37:31 -0400

    Feat: Update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git clone https://github.com/Serralq/algo-prog2.git
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-cd algo-prog2
 nix-shell --pure --run "./test.sh"

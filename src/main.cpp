0ba8e501259ac13d5796eff1339df8ce109ae823
Alexander Dao <alexanderngocdao@gmail.com>
2026-04-05 09:11:39 -0400

    Feat(main): Implement find max value using dp

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,14 @@ Table dp_func(std::string &A, std::string &B,
   int n = A.size(), m = B.size();
   Table dp(n + 1, std::vector<int>(m + 1, 0));
 
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+
+      if (A[i - 1] == B[j - 1])
+        dp[i][j] = std::max(dp[i][j], dp[i - 1][j - 1] + val[A[i - 1]]);
+    }
+  }
 
   return dp;
 }

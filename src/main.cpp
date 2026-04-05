c28577b2a3eb285010d61d22eea950a02256a690
Alexander Dao <alexanderngocdao@gmail.com>
2026-04-05 09:09:15 -0400

    Feat: Added expected shape of main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using Table = std::vector<std::vector<int>>;
+
+Table dp_func(std::string &A, std::string &B,
+              std::unordered_map<char, int> &val) {
+  int n = A.size(), m = B.size();
+  Table dp(n + 1, std::vector<int>(m + 1, 0));
+
+
+  return dp;
+}
+
+std::string backtrack(Table &dp, std::string &A, std::string &B,
+                      std::unordered_map<char, int> &val) {
+  std::string result;
+  return result;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+
+}
